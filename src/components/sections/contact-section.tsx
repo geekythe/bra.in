a7080b7d24987ba153e617f277db402e85c2f876
geekythe <geekyandbrain@gmail.com>
2025-06-30 13:44:23 +0300

    Update contact-section.tsx

--- src/components/sections/contact-section.tsx
+++ src/components/sections/contact-section.tsx
@@ -51,7 +51,7 @@ export default function ContactSection({ id }: SectionProps) {
             <div className="flex bg-red-500 justify-center mb-2">
               <SectionHeader title="REACH ME" />
             </div>
-            <div className="grid bg-blue-500 md-4 grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-2">
+            <div className="grid bg-blue-500 mb-4 grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-2">
               {/* Location */}
               <div className="flex flex-col items-center lg:flex-row p-2 rounded-lg">
                 <div className="w-16 h-16 md:w-20 md:h-20 flex items-center justify-center mb-2 lg:mb-0 flex-shrink-0">
